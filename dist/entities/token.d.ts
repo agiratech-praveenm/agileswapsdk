e0b430005b2e2af8566012100b241c7293ea33ad
coinfactory768 <coinfactory768@gmail.com>
2021-12-06 15:11:53 +0100

    updated

--- dist/entities/token.d.ts
+++ dist/entities/token.d.ts
@@ -25,6 +25,6 @@ export declare class Token extends Currency {
  */
 export declare function currencyEquals(currencyA: Currency, currencyB: Currency): boolean;
 export declare const WETH: {
-    56: Token;
+    25: Token;
     339: Token;
 };
